8c81c1127d7b3e8b7a0a4a09e35b83fe37604b76
ethanak <ethanak@polip.com>
2016-10-17 08:34:52 +0200

    Create ESP8266PgConsole.ino

---
+++ examples/ESP8266PgConsole/ESP8266PgConsole.ino
@@ -0,0 +1,224 @@
+/*
+ * SimplePgSQL.c - Lightweight PostgreSQL connector for ESP8266
+ * Copyright (C) Bohdan R. Rau 2016 <ethanak@polip.com>
+ *
+ * SimplePgSQL is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * SimplePgSQL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with SimplePgSQL.  If not, write to:
+ * 	The Free Software Foundation, Inc.,
+ * 	51 Franklin Street, Fifth Floor
+ * 	Boston, MA  02110-1301, USA.
+ */
+
+/*
+ * Demo program for SimplePgSQL library
+ * Simple PostgreSQL console
+ * Accepts:
+ * - PostgreSQL simple queries
+ * - \d - displays table list
+ * - \d tablename - list table columns
+ * - exit - closes connection
+ */
+
+#include <ESP8266WiFi.h>
+#include <SimplePgSQL.h>
+
+
+
+IPAddress PGIP(192,168,1,5);
+
+const char ssid[] = "YOUR_SSID";        //  your network SSID (name)
+const char pass[] = "YOUR_PASSWORD";    // your network password
+
+const char user[] = "db_user";          // your database user
+const char password[] = "db_password";  // your database password
+const char dbname[] = "arduino";        // your database name
+int WiFiStatus;
+
+WiFiClient client;
+PGconnection conn(&client);
+
+void setup(void)
+{
+    Serial.begin(115200);
+    WiFi.begin(ssid, pass);
+}
+
+void checkConnection()
+{
+    int status = WiFi.status();
+    if (status != WL_CONNECTED) {
+        if (WiFiStatus == WL_CONNECTED) {
+            Serial.println("Connection lost");
+            WiFiStatus = status;
+        }
+    }
+    else {
+        if (WiFiStatus != WL_CONNECTED) {
+            Serial.println("Connected");
+            WiFiStatus = status;
+        }
+    }
+}
+
+static PROGMEM const char query_rel[] = "\
+SELECT a.attname \"Column\",\
+  pg_catalog.format_type(a.atttypid, a.atttypmod) \"Type\",\
+  case when a.attnotnull then 'not null ' else '' end ||\
+  (SELECT substring(pg_catalog.pg_get_expr(d.adbin, d.adrelid) for 128)\
+   FROM pg_catalog.pg_attrdef d\
+   WHERE d.adrelid = a.attrelid AND d.adnum = a.attnum AND a.atthasdef) \"Extras\"\
+ FROM pg_catalog.pg_attribute a, pg_catalog.pg_class c\
+ WHERE a.attrelid = c.oid AND c.relkind = 'r' AND\
+ c.relname = %s AND\
+ pg_catalog.pg_table_is_visible(c.oid)\
+ AND a.attnum > 0 AND NOT a.attisdropped\
+    ORDER BY a.attnum";
+
+static PROGMEM const char query_tables[] = "\
+SELECT n.nspname as \"Schema\",\
+  c.relname as \"Name\",\
+  CASE c.relkind WHEN 'r' THEN 'table' WHEN 'v' THEN 'view' WHEN 'm' THEN 'materialized view' WHEN 'i' THEN 'index' WHEN 'S' THEN 'sequence' WHEN 's' THEN 'special' WHEN 'f' THEN 'foreign table' END as \"Type\",\
+  pg_catalog.pg_get_userbyid(c.relowner) as \"Owner\"\
+ FROM pg_catalog.pg_class c\
+     LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace\
+ WHERE c.relkind IN ('r','v','m','S','f','')\
+      AND n.nspname <> 'pg_catalog'\
+      AND n.nspname <> 'information_schema'\
+      AND n.nspname !~ '^pg_toast'\
+  AND pg_catalog.pg_table_is_visible(c.oid)\
+ ORDER BY 1,2";
+
+int pg_status = 0;
+
+void doPg(void)
+{
+    char *msg;
+    int rc;
+    if (!pg_status) {
+        conn.setDbLogin(PGIP,
+            user,
+            password,
+            dbname,
+            "utf8");
+        pg_status = 1;
+        return;
+    }
+
+    if (pg_status == 1) {
+        rc = conn.status();
+        if (rc == CONNECTION_BAD || rc == CONNECTION_NEEDED) {
+            char *c=conn.getMessage();
+            if (c) Serial.println(c);
+            pg_status = -1;
+        }
+        else if (rc == CONNECTION_OK) {
+            pg_status = 2;
+            Serial.println("Enter query");
+        }
+        return;
+    }
+    if (pg_status == 2) {
+        if (!Serial.available()) return;
+        char inbuf[64];
+        int n = Serial.readBytesUntil('\n',inbuf,63);
+        while (n > 0) {
+            if (isspace(inbuf[n-1])) n--;
+            else break;
+        }
+        inbuf[n] = 0;
+
+        if (!strcmp(inbuf,"\\d")) {
+            if (conn.execute(query_tables, true)) goto error;
+            Serial.println("Working...");
+            pg_status = 3;
+            return;
+        }
+        if (!strncmp(inbuf,"\\d",2) && isspace(inbuf[2])) {
+            char *c=inbuf+3;
+            while (*c && isspace(*c)) c++;
+            if (!*c) {
+                if (conn.execute(query_tables, true)) goto error;
+                Serial.println("Working...");
+                pg_status = 3;
+                return;
+            }
+            if (conn.executeFormat(true, query_rel, c)) goto error;
+            Serial.println("Working...");
+            pg_status = 3;
+            return;
+        }
+
+        if (!strcmp(inbuf,"exit",4)) {
+            conn.close();
+            Serial.println("Thank you");
+            pg_status = -1;
+            return;
+        }
+        if (conn.execute(inbuf)) goto error;
+        Serial.println("Working...");
+        pg_status = 3;
+    }
+    if (pg_status == 3) {
+        rc=conn.getData();
+        int i;
+        if (rc < 0) goto error;
+        if (!rc) return;
+        if (rc & PG_RSTAT_HAVE_COLUMNS) {
+            for (i=0; i < conn.nfields(); i++) {
+                if (i) Serial.print(" | ");
+                Serial.print(conn.getColumn(i));
+            }
+            Serial.println("\n==========");
+        }
+        else if (rc & PG_RSTAT_HAVE_ROW) {
+            for (i=0; i < conn.nfields(); i++) {
+                if (i) Serial.print(" | ");
+                msg = conn.getValue(i);
+                if (!msg) msg=(char *)"NULL";
+                Serial.print(msg);
+            }
+            Serial.println();
+        }
+        else if (rc & PG_RSTAT_HAVE_SUMMARY) {
+            Serial.print("Rows affected: ");
+            Serial.println(conn.ntuples());
+        }
+        else if (rc & PG_RSTAT_HAVE_MESSAGE) {
+            msg = conn.getMessage();
+            if (msg) Serial.println(msg);
+        }
+        if (rc & PG_RSTAT_READY) {
+            pg_status = 2;
+            Serial.println("Enter query");
+        }
+    }
+    return;
+error:
+    msg = conn.getMessage();
+    if (msg) Serial.println(msg);
+    else Serial.println("UNKNOWN ERROR");
+    if (conn.status() == CONNECTION_BAD) {
+        Serial.println("Connection is bad");
+        pg_status = -1;
+    }
+}
+
+
+void loop()
+{
+    checkConnection();
+    if (WiFiStatus == WL_CONNECTED) {
+        doPg();
+    }
+    delay(50);
+}
